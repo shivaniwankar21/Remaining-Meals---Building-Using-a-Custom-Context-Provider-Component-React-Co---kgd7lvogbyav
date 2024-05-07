a5e54407f07536465d2a92b493d0ca84154270a0
shivaniwankar21 <shivanikukankar@gmail.com>
2024-05-07 13:59:13 +0530

    Update Counter.js

--- pages/Counter.js
+++ pages/Counter.js
@@ -1,7 +1,12 @@
+import { useMeals } from "./MealsProvider";
+
 const Counter = () => {
+  const { meals } = useMeals();
+  const remainingMeals = meals.filter((meal) => !meal.ticked).length;
+
   return (
     <div>
-      <h3>Meals Remaining: remainingMeals</h3>
+      <h3>Meals Remaining: {remainingMeals}</h3>
     </div>
   );
 };
