6a5e414c38526aea34e50637d55523e0139bf82c
shivaniwankar21 <shivanikukankar@gmail.com>
2024-05-07 14:01:00 +0530

    Update MealsProvider.js

--- pages/MealsProvider.js
+++ pages/MealsProvider.js
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const todaysMeals = [
-  { id: 1, name: "Baked Beans" },
-  { id: 2, name: "Baked Sweet Potatoes" },
-  { id: 3, name: "Baked Potatoes" },
+  { id: 1, name: 'Baked Beans' },
+  { id: 2, name: 'Baked Sweet Potatoes' },
+  { id: 3, name: 'Baked Potatoes' },
 ];
+
+// Create a context
+const MealsContext = createContext();
 
 const MealsProvider = ({ children }) => {
   const [meals, setMeals] = useState(todaysMeals);
 
-  const tickMeal = (id) => {};
+  const tickMeal = (id) => {
+    setMeals((prevMeals) =>
+      prevMeals.map((meal) =>
+        meal.id === id ? { ...meal, ticked: !meal.ticked } : meal
+      )
+    );
+  };
 
-  return <div></div>;
+  return (
+    <MealsContext.Provider value={{ meals, tickMeal }}>
+      {children}
+    </MealsContext.Provider>
+  );
+};
+
+export const useMeals = () => {
+  return useContext(MealsContext);
 };
 
 export default MealsProvider;
